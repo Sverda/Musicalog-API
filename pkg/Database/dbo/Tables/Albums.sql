b8b5472a97f35f0a3752e583acbf5fd995fce93d
Damian Ubowski <damian.ubowski@jmr.pl>
2021-06-25 16:11:04 +0200

    Add database model project

---
+++ pkg/Database/dbo/Tables/Albums.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Albums] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Title]    VARCHAR (100) NOT NULL,
+    [ArtistId] INT           NOT NULL,
+    [Type]     INT           NOT NULL,
+    [Stock]    INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Album_Artists] FOREIGN KEY ([ArtistId]) REFERENCES [dbo].[Artists] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
---
+++ pkg/Database/dbo/Tables/Artists.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Artists] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Name] VARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
